d452c059ef7f26296e35b01f6aa225780a9b14b9
Starr <17998916+BorntobeStarr@users.noreply.github.com>
2021-06-28 15:32:28 +0000

    Part 2: Multiple Questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,12 +6,24 @@ const input = require('readline-sync');
 let candidateName;
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride"
-let candidateAnswer = ""
+let question = [
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters?", 
+  "(5 + 3)/2 * 10 = ?", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 
+  "What is the minimum crew size for the ISS?"
+  ];
+let correctAnswer = [
+  "Sally Ride", 	
+  "true", 
+  "40", 
+  "Trajectory", 
+  "3"];
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
-let candidateAnswers;
+let candidateAnswers= []
+
 
 
 function askForName () {
@@ -21,21 +33,22 @@ candidateName = input.question ("Please enter candidates name: ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-candidateAnswer = input.question (question);
-  if (candidateAnswer === "Sally Ride") {
-    console.log("Awesome Job! You chose the correct answer!")
+for (let i=0; i<question.length; i++) {
+candidateAnswer = input.question (question[i])
+  if (candidateAnswer.includes(correctAnswer[i])) {
+   console.log (`Great work ${candidateName}, ${candidateAnswer} is the correct answer!`)
   } 
   else {
-   console.log("Thank you for your input, the answer you provided was incorrect.");
+   console.log(`Thank you for your input ${candidateName}, the answer ${candidateAnswer} , you provided was incorrect.`);
+  } 
 }
 
-}
+} 
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
 
   let grade;
   
